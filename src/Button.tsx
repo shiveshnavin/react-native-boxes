d9d8338a02e074b98808b2b90df052c25f1c6c41
Shivesh Navin <you@example.com>
2024-11-06 02:42:07 +0530

    Update-files

--- src/Button.tsx
+++ src/Button.tsx
@@ -143,7 +143,7 @@ export function ButtonView(props: ButtonViewProps) {
                         }}>
                             <BtnIcon color={
                                 //@ts-ignore
-                                typeof props.icon == 'string' ? tstyle.color || theme.colors.invert.text : undefined} />
+                                typeof props.icon == 'string' ? tstyle.color || "#fff" : undefined} />
                         </View>
                     )
                 }
